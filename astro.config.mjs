6cbab2fd43dea77dc582b5abee2c65f205cf12cd
Edward Grant <edward.grant@vmlyr.com>
2026-05-14 18:55:59 -0400

    fixing config syntax for svgo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, svgoOptimizer } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://grwebdev.github.io',
   base: '/grwebdev-website-v2/',
   experimental: {
-    svgo: true,
+    svgOptimizer: svgoOptimizer(),
   }
 });
